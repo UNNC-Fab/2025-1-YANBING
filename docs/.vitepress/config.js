ca7342cb8a56c61fab5140b6ec7fd3d21a418bd7
Yanbing333 <suxyw2@nottingham.edu.cn>
2025-03-06 15:22:16 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base:'2025-1-YANBING',
+  base:'2025-1-yanbing',
   title: "我的文档网站",
   description: "使用VitePress构建的文档网站",
   themeConfig: {
