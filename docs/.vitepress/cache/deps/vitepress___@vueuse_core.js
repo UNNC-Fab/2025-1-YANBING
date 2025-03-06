ce2f8a911000cba09b56e6d3d9baf4c87b99a228
Yanbing333 <suxyw2@nottingham.edu.cn>
2025-03-06 14:12:09 +0800

    update yml

--- docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -29,6 +29,7 @@ import {
   createFilterWrapper,
   createGlobalState,
   createInjectionState,
+  createRef,
   createReusableTemplate,
   createSharedComposable,
   createSingletonPromise,
@@ -280,8 +281,8 @@ import {
   watchTriggerable,
   watchWithFilter,
   whenever
-} from "./chunk-YJ6LH72U.js";
-import "./chunk-QCLMCU2M.js";
+} from "./chunk-Z5QSWKN2.js";
+import "./chunk-LW4I4DCF.js";
 export {
   DefaultMagicKeysAliasMap,
   StorageSerializers,
@@ -317,6 +318,7 @@ export {
   createGlobalState,
   createInjectionState,
   reactify as createReactiveFn,
+  createRef,
   createReusableTemplate,
   createSharedComposable,
   createSingletonPromise,
